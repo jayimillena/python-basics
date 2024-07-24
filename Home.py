b8b55582b9baa2a7247e09902d0bdbf5f8cc17b7
Jay Millena <jay.millena07@gmail.com>
2024-07-24 16:18:20 +0800

    Variables and datatypes using Python

--- Home.py
+++ Home.py
@@ -1,4 +1,9 @@
-print('Jay Millena')
-print('o-----')
-print('  ||||')
-print('*' * 20)
+name = "John Smith"
+age = 30
+is_new_patient = True
+if is_new_patient:
+    print('We check in a patient named ' + name + '. ' + 'He is ' + str(age) + ' years old.' + ' and is a new patient.')
+else:
+    print('We check in a patient named ' + name + '. ' + 'He is ' + str(age) + ' years old.' + ' and is an old patient.')
+
+
