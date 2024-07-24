b18a4760e47cc8e1af818d4b349262e9f3f58cda
Jay Millena <jay.millena07@gmail.com>
2024-07-24 16:21:59 +0800

    Input function

--- Home.py
+++ Home.py
@@ -1,9 +1,2 @@
-name = "John Smith"
-age = 30
-is_new_patient = True
-if is_new_patient:
-    print('We check in a patient named ' + name + '. ' + 'He is ' + str(age) + ' years old.' + ' and is a new patient.')
-else:
-    print('We check in a patient named ' + name + '. ' + 'He is ' + str(age) + ' years old.' + ' and is an old patient.')
-
-
+name = input('What is your name? ')
+print('Hi ' + name)
